c7b20f840d193a4bc137a2ed3eb241e309cd0bbd
GarriGarrison <garrisonford@yandex.ru>
2022-12-09 20:10:55 +0300

    сделана обработка вывода

---
+++ src/out/console-logger/console-logger.ts
@@ -0,0 +1,25 @@
+import { IStreamLogger } from '../../core/handlers/stream-logger.interface'
+
+export class ConsoleLogger implements IStreamLogger {
+  private static logger: ConsoleLogger
+
+  public static getInstance() {
+    if (!ConsoleLogger.logger) {
+      ConsoleLogger.logger = new ConsoleLogger()
+    }
+
+    return ConsoleLogger.logger
+  }
+
+  public log(...args: any[]): void {
+    console.log(...args)
+  }
+
+  public error(...args: any[]): void {
+    console.error(...args)
+  }
+
+  public end(): void {
+    console.log('Готово')
+  }
+}
