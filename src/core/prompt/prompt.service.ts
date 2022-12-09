5cff7bb16a30ce70a60d6824bf91eddb419914a6
GarriGarrison <garrisonford@yandex.ru>
2022-12-09 19:34:19 +0300

    сделана обработка ввода

---
+++ src/core/prompt/prompt.service.ts
@@ -0,0 +1,16 @@
+import inqueirer from 'inquirer'
+import { PromptType } from './prompt.types'
+
+export class PromptService {
+  public async input<T>(message: string, type: PromptType) {
+    const { result } = await inqueirer.prompt<{result: T}>([
+      {
+        type: 'input',
+        name: 'result',
+        message,
+      },
+    ])
+
+    return result
+  }
+}
